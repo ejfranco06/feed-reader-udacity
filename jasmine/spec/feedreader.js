cc6f6569601d652381a402d8e9b564721a3cfc26
Emilio J. Franco <ejfranco06@gmail.com>
2018-08-04 16:12:21 -0400

    Add menu is hidden initially test spec

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -57,18 +57,29 @@ $(function() {
 
 
   /* TODO: Write a new test suite named "The menu" */
+  describe('The menu', function() {
+    const bodyEl = document.getElementsByTagName('body')[0];
 
-  /* TODO: Write a test that ensures the menu element is
-   * hidden by default. You'll have to analyze the HTML and
-   * the CSS to determine how we're performing the
-   * hiding/showing of the menu element.
-   */
+    /* TODO: Write a test that ensures the menu element is
+    * hidden by default. You'll have to analyze the HTML and
+    * the CSS to determine how we're performing the
+    * hiding/showing of the menu element.
+    */
 
-  /* TODO: Write a test that ensures the menu changes
-   * visibility when the menu icon is clicked. This test
-   * should have two expectations: does the menu display when
-   * clicked and does it hide when clicked again.
-   */
+    it('is initialy hidden', function() {
+      const classList = bodyEl.classList;
+      expect(classList.contains('menu-hidden')).toBe(true);
+
+    });
+
+
+    /* TODO: Write a test that ensures the menu changes
+    * visibility when the menu icon is clicked. This test
+    * should have two expectations: does the menu display when
+    * clicked and does it hide when clicked again.
+    */
+  });
+
 
   /* TODO: Write a new test suite named "Initial Entries" */
 
